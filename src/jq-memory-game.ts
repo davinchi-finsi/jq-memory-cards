71b7d7a7e17c423568b38247240074107cd3721b
Javier Pérez <javier.perez@davinchi.es>
2020-09-07 10:18:31 +0200

    feat: implemented autoPair option, true by default
    When autoPair = true, the component will automatically create the pairs for each items by cloning the card, just like is now working.
    
    When autoPair = false, is up to the dev to implement manually all the items. The component will verify each id has only 2 elements and report an error otherwise

--- src/jq-memory-game.ts
+++ src/jq-memory-game.ts
@@ -19,6 +19,7 @@
             selector: {
                 interactions: "[data-jq-memory-game-interactions], [jq-memory-game-interactions]"
             },
+            autoPair: true,
             classes: {
                 root: "jq-memory-game",
                 disabled: "jq-memory-game--disabled",
@@ -39,9 +40,11 @@
         _renderCard: function (item) {
             let back;
             let front;
+            // provide src to render an image
             if (item?.front?.src != null) {
                 front = `<img src="${item.front.src}" alt="${item.front.alt}"/>`;
             } else {
+                // if the property is a function, invoke, otherwise just add the content. Could be a jquery selector
                 front = (typeof item.front) == "function" ? item.front(this, item) : item.front
             }
             if (item?.back?.src != null) {
@@ -66,19 +69,55 @@
             return $card;
         },
         _generateCards: function () {
+            // generate the data for the cards
             this._cards = this.options.data.reduce((acc, item) => {
+                // render the card
                 const $card = this._renderCard(item);
+                // add the id of the card to be able to retrieve it
                 $card.data("card-id", item.id);
                 acc.push({
                     id: item.id,
                     $card
                 });
-                acc.push({
-                    id: item.id,
-                    $card: $card.clone()
-                });
+                // when autoPair is true, automatically generate the pair
+                if (this.options.autoPair) {
+                    acc.push({
+                        id: item.id,
+                        $card: $card.clone()
+                    });
+                }
                 return acc;
             }, []);
+            // when autoPair false, verify all the cards have their pair (only one)
+            if (!this.autoPair) {
+                // group by id
+                const pairs = this._cards.reduce(
+                    (acc, item) => {
+                        let group = acc[item.id];
+                        if (group == null) {
+                            group = [];
+                            acc[item.id] = group;
+                        }
+                        group.push(item);
+                        return acc;
+                    },
+                    {}
+                );
+                // each id must have 2 items
+                let errors = [];
+                for (let id in pairs) {
+                   const group = pairs[id];
+                   if (group.length != 2) {
+                       errors.push(`- id '${id}' has ${group.length} item/s (exactly 2 items required)`)
+                   }
+                }
+                if (errors.length > 0) {
+                    throw new Error(
+                        "When the option 'autoPair' = false, all the items (cards) must be specified, 2 items for each id, but an incorrect number of items has been found:\n"+
+                        errors.join("\n")
+                    );
+                }
+            }
         },
         _renderBoard: function () {
             this._$board = $(`<div class="${this.options.classes.board}"></div>`);
@@ -87,6 +126,7 @@
         _initialize: function () {
             this._renderBoard();
             this._generateCards();
+            // delegate the click of the cards to the board
             this._$board.delegate("." + this.options.classes.card,
                 "click." + this.NAMESPACE,
                 this._onCardClick.bind(this));
@@ -98,6 +138,7 @@
                 this._$interactions.text(this._interactions);
             }
         },
+        // randomize the cards
         _shuffle: function (array) {
             for (let i = array.length - 1; i > 0; i--) {
 
@@ -118,18 +159,24 @@
             this._disableInteractions(true);
             this._updateInteractions(this._interactions+1);
             const [cardA, cardB] = this._flipped;
+            // check if the two selected cards matches
             if (cardA.id == cardB.id) {
                 cardA.$card.addClass(this.options.classes.match);
                 cardB.$card.addClass(this.options.classes.match);
+                // store the matched cards
                 this._match = this._match.concat(this._flipped);
             } else {
+                // if cards doesn't match, reset the position after a specified time
                 setTimeout(() => {
                     cardA.$card.removeClass(this.options.classes.flipped);
                     cardB.$card.removeClass(this.options.classes.flipped);
                 }, this.options.timeToWaitBetweenInteractions);
             }
+            // emit custom event
             this.element.trigger(this.ON_CARDS_CHECK, [this, cardA.id == cardB.id, this._interactions, this._flipped.slice()]);
+            // reset flipped cards
             this._flipped = [];
+            // check if all the cards has been paired
             if (this._match.length == this._cards.length) {
                 this.element.addClass(this.options.classes.completed);
                 this._completed = true;
@@ -140,6 +187,7 @@
                 }, this.options.timeToWaitBetweenInteractions);
             }
         },
+        // disable interactions with the cards without disabling the component
         _disableInteractions: function (disable) {
             this._disableInteraction = disable;
             if (disable) {
@@ -151,14 +199,18 @@
         getInteractions : function() {
             return this._interactions;
         },
+        // flip a card by iindex
         flip: function (index) {
             if (!this.options.disabled && !this._disableInteraction && !this._completed) {
+                // verify the card for the specified index exists
                 if (index >= 0 && (index - 1) <= this._cards.length) {
                     const cardItem = this._cards[index];
-                    this.element.trigger(this.ON_CARD_FLIPPED, [this, cardItem]);
                     const $card = cardItem.$card;
                     $card.addClass(this.options.classes.flipped);
                     this._flipped.push(cardItem);
+                    // trigger custom event
+                    this.element.trigger(this.ON_CARD_FLIPPED, [this, cardItem]);
+                    // when 2 cards are flipped, check if matches
                     if (this._flipped.length >= 2) {
                         this._checkCards();
                     }
@@ -167,6 +219,7 @@
                 }
             }
         },
+        // reset the game
         reset() {
             this._disableInteraction = false;
             this._flipped = [];
